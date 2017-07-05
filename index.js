a1f5a07c47831cef95429d72a5e7bf8859f0c85e
liluolpy <2410533176@qq.com>
2017-07-05 13:58:59 +0800

    修改过的

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ new Swiper('.swiper-container',{
 		pagination:'.swiper-pagination',
 		paginationClickable:true
 	});
+$('.centerAdd').on('click',function(){
+	$('.mark').fadeIn();
+	$('.animated').addClass('bounceInDown').removeClass('bounceOutDown')
+})
